a8dcf6ea498327f2c75f405f798cb62576e31e2a
nologyance <nologyance>
2024-02-08 23:43:45 +0900

    型を付ける

---
+++ src/common/FlexMessage.ts
@@ -0,0 +1,19 @@
+export const BOX = "box" as const;
+
+export const BASELINE = "baseline" as const;
+
+export const TEXT = "text" as const;
+
+export const BOLD = "bold" as const;
+
+export const BUBBLE = "bubble" as const;
+
+export const VERTICAL = "vertical" as const;
+
+export const SPACE = {
+  xl: "xl",
+  sm: "sm",
+  lg: "lg",
+  xxl: "xxl",
+} as const;
+
