04221fddb77e897a54a7e13ecc180c9f1ab0e5d2
nologyance <nologyance>
2024-02-08 23:45:31 +0900

    移動

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { askScheduleToGoHome } from "./AskScheduleToGoHome";
-import { routineInfo } from "./routineInfo/RoutineInfo";
 import { noticeScheduleToGoHome } from "./NoticeScheduleToGoHome";
-import { lineWebhook } from "./Webhook";
+import { routineInfo } from "./routineInfo/RoutineInfo";
+import { lineWebhook } from "./webhook/Webhook";
 
 exports.lineWebhook = lineWebhook;
 exports.askScheduleToGoHome = askScheduleToGoHome;
